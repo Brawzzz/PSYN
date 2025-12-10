19d300826e62cf50b48650ec06b2a7e57d5720e9
Oscar Ma <oscarma230@gmail.com>
2025-12-10 19:54:15 +0100

    Premiers resultats

---
+++ src/contours_detection.py
@@ -0,0 +1,38 @@
+import cv2 as cv 
+import numpy as np
+import setup as stp
+
+from setup import RED, GREEN, BLUE
+
+blur_path = stp.CNTRS_OUTPUT_PATH + 'blur.jpg'
+bw_path = stp.CNTRS_OUTPUT_PATH + 'bw.jpg'
+
+edges_path = stp.CNTRS_OUTPUT_PATH + 'edges.jpg'
+green_edges_path = stp.CNTRS_OUTPUT_PATH + 'green_edges.jpg'
+
+img_name = stp.BEFORE_FRET + stp.IMG_NAME
+
+img = cv.imread(img_name, cv.IMREAD_GRAYSCALE)
+img = cv.resize(src=img, dsize=None, fx=0.05, fy=0.05, interpolation=cv.INTER_AREA)
+
+#-------------------------- IMAGE PROCESSING --------------------------# 
+img_blur = cv.GaussianBlur(img, (9, 9), 0)
+cv.imwrite(blur_path, img_blur)
+
+(ret, img_bw) = cv.threshold(img_blur, stp.TH_MIN, stp.TH_MAX, cv.THRESH_BINARY + cv.THRESH_OTSU)
+cv.imwrite(bw_path, img_bw)
+
+#-------------------------- EDGE DETECTION --------------------------#
+img_edges = cv.Canny(image=img_bw, threshold1=150, threshold2=255) 
+
+img_rgb_edges = cv.cvtColor(img_edges, cv.COLOR_GRAY2RGB) 
+img_rgb_edges *= np.array((0, 1, 0), np.uint8)
+img_rgb = cv.cvtColor(img, cv.COLOR_GRAY2RGB)
+
+img_green_edges = cv.addWeighted(img_rgb, 0.25, img_rgb_edges, 0.75, 0)
+
+cv.imwrite(edges_path, img_edges)
+cv.imwrite(green_edges_path, img_green_edges)
+
+
+
---
+++ src/setup.py
@@ -0,0 +1,54 @@
+#--------------------------------------------------------------------------------------------------------------------#
+# ------------------------------------------------------- PATH ------------------------------------------------------#
+#--------------------------------------------------------------------------------------------------------------------#
+DATA_PATH = "./Images/"
+OUTPUT_PATH = "./Output/"
+
+CNTRS_OUTPUT_PATH = OUTPUT_PATH + "contours/" 
+
+# ------------------------------ SAMPLE ------------------------------%
+SAMPLE_FOLDER = "01"
+SAMPLE_INDEX = "01"
+SELECTED_SAMPLE = DATA_PATH + "sample_" + SAMPLE_FOLDER + "/"
+
+AFTER_FRET = SELECTED_SAMPLE + "after_fretting/"
+BEFORE_FRET = SELECTED_SAMPLE + "before_fretting/"
+BIN_MASKS = SELECTED_SAMPLE + "binary_masks/"
+
+SUFFIXE = "_pre"
+IMG_EXTENSION = ".jpg"
+
+IMG_NAME = "hxtl_p" + SAMPLE_INDEX + SUFFIXE + IMG_EXTENSION
+
+#--------------------------------------------------------------------------------------------------------------------#
+#----------------------------------------------------- CONSTANT -----------------------------------------------------#
+#--------------------------------------------------------------------------------------------------------------------#
+RED     = (0, 0, 255)
+GREEN   = (0, 255, 0)
+BLUE    = (255, 0, 0)
+
+#---------- PARAMS   ----------#
+AUTO_DETECTION = False
+
+TH_MIN = 0
+TH_MAX = 255
+
+ALPHA_X = 10
+ALPHA_Y = 10
+
+if(AUTO_DETECTION):
+    THLD_CONTOURS_LEN = 195
+else:
+    THLD_CONTOURS_LEN = 500
+
+X0 = 2200
+Y0 = 300
+W_W = 2000
+W_H = 800
+
+RECT_SIZE_HORIZONTALE = 150
+RECT_SIZE_VERTICALE = 90
+
+MIN_AREA = 500
+DELTA = 10
+
